ef7432796206f0bb265bc966586bfbb3c3898c92
Jggrau <jggrau@uoc.edu>
2021-03-31 08:48:27 +0200

    galeria

--- app/src/main/java/com/example/android/myapplication/Gallery.kt
+++ app/src/main/java/com/example/android/myapplication/Gallery.kt
@@ -2,10 +2,18 @@ package com.example.android.myapplication
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class Gallery : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.galeria)
     }
+}
+
+// get reference to ImageView
+//val image_view = findViewById(R.id.image_view) as ImageView
+// set on-click listener for ImageView
+//image_view.setOnClickListener {
+    // codigo que ejecuta
 }
